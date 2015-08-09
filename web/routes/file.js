9ccf3d855e8b16b05c4562c00142e6da2aa90e98
Jonathan Cremin <jonathan@crem.in>
2015-08-09 20:42:52 +0100

    Add content-length to downloads

--- web/routes/file.js
+++ web/routes/file.js
@@ -65,6 +65,7 @@ export function* get(id, name, size) {
   }
 
   this.set('Content-type', type);
+  this.set('Content-length', file.file_size);
   this.set('Expires', new Date(2020, 1).toISOString());
   this.set('Cache-control', 'max-age=2592000');
 
