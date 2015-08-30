e513c12becc08b9354d1052ae3b42cbe1f620fe8
Jonathan Cremin <jonathan@crem.in>
2015-08-30 23:06:49 +0200

    Update jspm dependencies

--- web/public/config.js
+++ web/public/config.js
@@ -12,21 +12,21 @@ System.config({
   },
 
   map: {
-    "angular": "npm:angular@1.4.4",
+    "angular": "npm:angular@1.4.5",
     "angular-reconnecting-websocket": "github:adieu/angular-reconnecting-websocket@0.1.1",
     "angular-strap": "npm:angular-strap@2.3.1",
-    "angular/resource": "npm:angular-resource@1.4.4",
-    "angular/route": "npm:angular-route@1.4.4",
-    "babel": "npm:babel-core@5.8.22",
+    "angular/resource": "npm:angular-resource@1.4.5",
+    "angular/route": "npm:angular-route@1.4.5",
+    "babel": "npm:babel-core@5.8.23",
     "babel-runtime": "npm:babel-runtime@5.8.20",
     "bootstrap-sass": "npm:bootstrap-sass@3.3.5",
     "cferdinandi/smooth-scroll": "github:cferdinandi/smooth-scroll@5.3.7",
-    "core-js": "npm:core-js@1.1.1",
+    "core-js": "npm:core-js@1.1.3",
     "dropzone": "npm:dropzone@4.0.1",
     "jquery": "npm:jquery@2.1.4",
     "zeroclipboard": "npm:zeroclipboard@2.2.0",
     "github:jspm/nodelibs-buffer@0.1.0": {
-      "buffer": "npm:buffer@3.4.2"
+      "buffer": "npm:buffer@3.4.3"
     },
     "github:jspm/nodelibs-path@0.1.0": {
       "path-browserify": "npm:path-browserify@0.0.0"
@@ -41,18 +41,18 @@ System.config({
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
-    "npm:angular@1.4.4": {
+    "npm:angular@1.4.5": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:babel-runtime@5.8.20": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
-    "npm:buffer@3.4.2": {
+    "npm:buffer@3.4.3": {
       "base64-js": "npm:base64-js@0.0.8",
       "ieee754": "npm:ieee754@1.1.6",
       "is-array": "npm:is-array@1.0.1"
     },
-    "npm:core-js@1.1.1": {
+    "npm:core-js@1.1.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
