84c41761c3570d0867c56ec534d6a96e95146b50
Jonathan Cremin <jonathan@crem.in>
2026-05-30 16:33:55 +0100

    Send auth with form for test

--- Containerfile
+++ Containerfile
@@ -12,7 +12,7 @@ COPY . .
 
 RUN pnpm run build
 
-ENV PORT 3000
+ENV PORT=3000
 EXPOSE 3000
 
 CMD ["pnpm", "start"]
--- test/web/user.spec.js
+++ test/web/user.spec.js
@@ -6,32 +6,34 @@ const request = agent(getRequestListener(app.fetch));
 
 describe('hostr-web user', function() {
   describe('when POST /signin with invalid credentials', function() {
-    it('should not redirect to /', function() {
+    it('should not redirect to /', function(done) {
       request.get('/signin').end(function(err, response) {
+        if (err) return done(err);
         const match = response.text.match(/name="_csrf" value="([^"]+)"/);
         const csrf = match[1];
         request
           .post('/signin')
+          .type('form')
           .send({'email': 'test@hostr.co', 'password': 'test-passworddeded', '_csrf': csrf})
-          .expect(200);
+          .expect(200)
+          .end(done);
       });
     });
   });
 
   describe('when POST /signin with valid credentials', function() {
-    it('should redirect to /', function() {
+    it('should redirect to /', function(done) {
       request.get('/signin').end(function(err, response) {
+        if (err) return done(err);
         const match = response.text.match(/name="_csrf" value="([^"]+)"/);
         const csrf = match[1];
         request
           .post('/signin')
+          .type('form')
           .send({'email': 'test@hostr.co', 'password': 'test-password', '_csrf': csrf})
           .expect(302)
           .expect('Location', '/')
-          .end(function() {
-            // Hax
-            process.exit();
-          });
+          .end(done);
       });
     });
   });
